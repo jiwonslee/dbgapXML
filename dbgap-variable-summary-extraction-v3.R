55fd2b22b623dc051de08b8e64b3b3b0cce6b4fc
Jiwon Lee <jlee250@bwh.harvard.edu>
2018-11-06 14:33:33 -0500

    adding version patch and consent as separate columns

--- dbgap-variable-summary-extraction-v3.R
+++ dbgap-variable-summary-extraction-v3.R
@@ -19,11 +19,10 @@ if (!is.na(table)){
 }else{
   message('warning: no --table set. Information of all tables are being extracted')
 }
-#args = commandArgs(trailingOnly=TRUE)
-#study = args[1]
-#table = args[2]
+###tested with#####
 #study = 'phs000007'
 #table = 'pht000024'
+
 dir = argv$dir
 if (!is.na(dir)){
   setwd(dir)
@@ -33,6 +32,16 @@ base_url = 'ftp://ftp.ncbi.nlm.nih.gov/dbgap/studies/'
 study_url = paste0(base_url, study)
 print(paste("study url", study_url))
 
+return_consent <- function(var_parts){
+  if (length(var_parts)==3){
+    consent='All'
+  }
+  else{
+    consent=var_parts[4]
+  }
+  return(consent)
+}
+
 ###### needs suffix / for the next line to work...
 folders <- unlist(strsplit(RCurl::getURL(paste0(study_url, '/'), ftp.use.epsv = TRUE, dirlistonly = TRUE), "\n"))
 
@@ -40,7 +49,6 @@ max_v = -1
 max_p = -1
 for (i in 1:length(folders)) {
     current = unlist(strsplit(folders[i], "\\."))
-    
     if (current[1] == study) {
         current_v = as.numeric(substring(current[2], 2))
         current_p = as.numeric(substring(current[3], 2))
@@ -71,11 +79,6 @@ message(paste0('extracting information from ', length(xml_path_suffixes), ' file
 #for (i in 1:length(xml_path_suffixes)) {
 for (i in 1:length(xml_path_suffixes)) {
     xml_url = paste0(main_url, xml_path_suffixes[i])
-    
-    # data_dict and var_report do and don't contain 'pNN' patch levels of table versions:
-    # phs000007.v29.pht004361.v2.l_bamyl_ex02_3b_0818s.data_dict.xml
-    # phs000007.v29.pht004361.v2.p10.l_bamyl_ex02_3b_0818s.var_report.xml
-    
     current = unlist(strsplit(xml_path_suffixes[i], "\\."))
     # don't parse meta data xml
     if (length(current) > 5) {
@@ -102,17 +105,17 @@ for (i in 1:length(xml_path_suffixes)) {
             message('error in table name format - check and modify code')
             quit()
         }
-
         xml = read_xml(RCurl::getURLContent(xml_url))
-
         recs <- xml_find_all(xml, "//variable")
         vals = trimws(xml_text(recs))
-        table_parts <- as.vector(str_split(xml_path_suffixes[i], '\\.', simplify=TRUE))
-        # extract and clean (if needed) the area names
-        variable_ids <- trimws(xml_attr(recs, "id"))
-        descriptions <- trimws(xml_text(xml_find_all(xml, "//variable//description")))
+        full_variable_id <- trimws(xml_attr(recs, "id"))
+        phv_parts <- sapply(full_variable_id, str_split, pattern='\\.')
+        variable_id<- sapply(phv_parts, function(x){return(x[1])})
+        phv_version <- sapply(phv_parts, function(x){return(x[2])})
+        description <- trimws(xml_text(xml_find_all(xml, "//variable//description")))
         if (doc=='var_report'){
-          
+          phv_patch <- sapply(phv_parts, function(x){return(x[3])})
+          consent <- sapply(phv_parts, return_consent)
           variable_name <- trimws(xml_attr(recs, "var_name"))
           type <- trimws(xml_attr(recs, "calculated_type"))
           units <- trimws(xml_attr(recs, 'units'))
@@ -125,19 +128,18 @@ for (i in 1:length(xml_path_suffixes)) {
           median <- trimws(xml_attr(stat, 'median'))
           min <- trimws(xml_attr(stat, 'min'))
           max <- trimws(xml_attr(stat, 'max'))
-          df <- cbind(variable_ids, variable_name, type , units, descriptions, n, nulls, mean, sd, median, min, max, study, study_version, table, table_version, table_name, table_patch)
+          df <- cbind(variable_id, phv_version, phv_patch, consent, variable_name, type , 
+                      units, description, n, nulls, mean, sd, median, min, max, 
+                      study, study_version, table, table_version, table_patch, table_name)
         }
         else if (doc=='data_dict'){
-          phv_parts <- unlist(sapply(variable_ids, str_split, pattern='\\.'))
-          phv <- phv_parts[seq(1, length(phv_parts)), by=2]
-          phv_version <- phv_parts[seq(2, length(phv_parts)), by=2]
+
           variable_name <- trimws(xml_text(xml_find_first(recs, 'name')))
           units <- trimws(xml_text(xml_find_first(recs, 'unit')))
           type <- trimws(xml_text(xml_find_first(recs, 'type')))
-          df <- cbind(phv, phv_version, variable_name, units, descriptions, study, study_version, table, table_version, table_name)
+          df <- cbind(variable_id, phv_version, variable_name, units, description, study, study_version, table, table_version, table_name)
         }
         fname <- str_replace(xml_path_suffixes[i], ".xml", ".txt")
         write.table(df, sep='\t', file=fname, row.names=FALSE, quote=FALSE)
-        
     }
 }
